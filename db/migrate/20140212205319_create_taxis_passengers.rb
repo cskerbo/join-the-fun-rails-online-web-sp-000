36e4d7635948a689b39f5e8472922c6588690566
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-15 15:06:46 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205319_create_taxis_passengers.rb
+++ db/migrate/20140212205319_create_taxis_passengers.rb
@@ -1,4 +1,5 @@
-create_join_table :taxis, :passengers do |t|
+class CreateTaxisPassengers def change
+  create_join_table :taxis, :passengers do |t|
   t.index :taxi_id
   t.index :passenger_id
 end
